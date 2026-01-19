0d31ba872c9f877482538dd3372d5e501f648d6e
Marcos Allan <106402058+Marcos-Allan@users.noreply.github.com>
2026-01-19 16:22:46 -0300

    Inicialização do projeto

---
+++ delivery/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className={`bg-red-600 w-dvw h-dvh flex items-center justify-center`}>
+     teste
+    </div>
+  )
+}
+
+export default App
