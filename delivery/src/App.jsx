56a50bbf192ceeadcf58476b0386d97f6909d970
Marcos Allan <106402058+Marcos-Allan@users.noreply.github.com>
2026-01-21 16:38:20 -0300

    Criei a tela inicial do app

--- delivery/src/App.jsx
+++ delivery/src/App.jsx
@@ -2,8 +2,23 @@ import './App.css'
 
 function App() {
   return (
-    <div className={`bg-red-600 w-dvw h-dvh flex items-center justify-center`}>
-     teste
+    <div
+      className={`bg-[#fefefe] w-dvw h-dvh flex flex-col items-center justify-start px-4 py-8 uppercase overflow-hidden`}
+    >
+     <p className={`text-[36px] mb-14`}>vairton</p>
+     <div className={`relative flex items-center justify-center w-[90%] rounded-3xl overflow-hidden border border-`}>
+      <p className={`left-0 absolute w-[50%] opacity-[0.7] h-full text-center py-2 bg-[#7d5bf7] rounded-3xl px-12`}></p>
+      <p className={`grow h-full py-2 pr-12 text-right`}>div8919</p>
+      <p className={`grow h-full py-2 pr-12 text-right`}>vairton</p>
+     </div>
+
+     <div className={`text-[32px] font-bold border my-auto border-black w-[80vw] h-[80vw] rounded-full flex items-center justify-center`}>
+      iniciar
+     </div>
+
+     <p className={`border border-black w-[85%] py-4 flex items-center justify-center rounded-[60px]`}>
+      palca - div8919
+     </p>
     </div>
   )
 }
